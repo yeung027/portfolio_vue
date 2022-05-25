4710bfb29e34b97640727abf720d9952261784a9
yeung027 <shyeung_sfo@wfsfaa.gov.hk>
2022-05-25 16:18:50 +0800

    type animation, style, color

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,26 @@
 <template>
 <div class="h-screen items-center justify-center flex-col">
-  <div :class="$style.highhigh"></div>
-  <section class="flex items-center justify-center w-screen text-base text-lg gap-1" v-observe-visibility="introVisibilityChanged">
-  <span class="">begin</span>
-  <span class="overflow-x-hidden group-hover:animate-type-reverse whitespace-nowrap text-brand-accent"  
-        :class="[introVisible ? $style.type : '']"
+  <section 
+    class="flex flex-col items-center justify-center w-screen h-screen bg-clip-border bg-center-top bg-scroll bg-cover ]" 
+    v-observe-visibility="introVisibilityChanged"
+    :class="$style.intro"
   >
-    hello!dsasss
+  <div class="w-screen pl-6 text-white font-medium text-3xl">
+    Alex Smith
+  </div>
+  <div class="w-screen pl-6 flex flex-row gap-2 text-white font-normal text-2xl pt-3 tracking-wide">
+  <span class="">I'm</span>
+  <span class="overflow-x-hidden group-hover:animate-type-reverse whitespace-nowrap text-brand-accent border-b-3 border-emerald-400"  
+        :class="[introVisible ? $style.type_reverse : '']"
+  >
+    Stupid
     </span>
+
   <span
-      class="box-border inline-block h-4 w-1 bg-black  "
+      class="box-border inline-block h-5 w-0.5 bg-black my-1.5"
       :class="$style.cursor"
     ></span>
+  </div>
   </section>
 </div>
 </template>
@@ -19,17 +28,17 @@
 <script lang="ts">
 import Vue from 'vue'
 
- declare interface BaseComponentData {
-  isMobileMenuOpen:boolean
-  isDarkMode:boolean
-  introVisible: boolean
-}
+//  declare interface BaseComponentData {
+//   isMobileMenuOpen:boolean
+//   isDarkMode:boolean
+//   introVisible: boolean
+// }
 
 import { ObserveVisibility } from 'vue-observe-visibility'
 Vue.directive('observe-visibility', ObserveVisibility)
 export default Vue.extend({
   name: 'IndexPage',
-  data(): BaseComponentData {
+  data()/*: BaseComponentData*/ {
     return {
       isMobileMenuOpen: false,
       isDarkMode: false,
@@ -50,6 +59,12 @@ export default Vue.extend({
 })
 </script>
 <style module>
+  section.intro
+  {
+    background-image: url('~@/static/img/hero-bg.jpg');
+    background-position:  60% 0%;
+  }
+
   .highhigh
   {
     width: 100%;
@@ -59,9 +74,8 @@ export default Vue.extend({
   .cursor 
   {
     display: block;
-    background-color: #000;
+    background-color: #fff;
     animation: cursor 0.6s linear infinite alternate;
-    /* will-change: opacity; */
   }
 
   @keyframes cursor 
@@ -80,7 +94,6 @@ export default Vue.extend({
   .type 
   {
     animation: type 1.8s ease-out .8s 1 normal both;
-    will-change: opacity;
   }
 
   @keyframes type {
@@ -88,12 +101,17 @@ export default Vue.extend({
     5%, 10% { width: 1ch }
     15%, 20% { width: 2ch }
     25%, 30% { width: 3ch }
-    35%, 40% { width: 4ch }
-    45%, 50% { width: 5ch }
-    55%, 60% { width: 6ch }
-    65%, 70% { width: 7ch }
-    75%, 80% { width: 8ch; }
-    85%, 90% { width: 9ch }
-    95% { width: 10ch }
+    35%, 40% { width: 3ch }
+    45%, 50% { width: 4ch }
+    55%, 60% { width: 4ch }
+    65%, 70% { width: 4ch }
+    75%, 80% { width: 5.5ch; }
+    85%, 90% { width: 5.5ch }
+    95% { width: 5.5.5ch }
+  }
+
+  .type_reverse
+  {
+    animation: type 1.8s ease-out 0s infinite alternate-reverse both
   }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	theme: {
-        extend: {
-          animation: {
-            cursor: 'cursor .6s linear infinite alternate',
-            type: 'type 1.8s ease-out .8s 1 normal both',
-            'type-reverse': 'type 1.8s ease-out 0s infinite alternate-reverse both',
-          },
-          keyframes: {
-            type: {
-              '0%': { width: '0ch' },
-              '5%, 10%': { width: '1ch' },
-              '15%, 20%': { width: '2ch' },
-              '25%, 30%': { width: '3ch' },
-              '35%, 40%': { width: '4ch' },
-              '45%, 50%': { width: '5ch' },
-              '55%, 60%': { width: '6ch' },
-              '65%, 70%': { width: '7ch' },
-              '75%, 80%': { width: '8ch' },
-              '85%, 90%': { width: '9ch' },
-              '95%': { width: '10ch' },
-            },
-          },
+        borderWidth: {
+          DEFAULT: '1px',
+          '0': '0',
+          '2': '2px',
+          '3': '3px',
+          '4': '4px',
+          '6': '6px',
+          '8': '8px',
         },
-      },
+        extend: {
+            colors: {
+              amber: colors.amber,
+              emerald: colors.emerald,
+            }
+          }
+      }
 }
