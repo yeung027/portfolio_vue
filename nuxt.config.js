04a053db807e993a80020278f841cbf2d39ae08c
yeung027 <shinghei.yeung@gmail.com>
2022-05-29 16:18:38 +0800

    target static to service

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  target: 'static',
+  target: 'service',
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
